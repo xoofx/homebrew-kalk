88209825cb3b6debb25dd8ff7fa74b9c03b4b970
Alexandre Mutel <alexandre_mutel@live.com>
2020-12-06 18:24:17 +0100

    0.3.1

--- Formula/kalk.rb
+++ Formula/kalk.rb
@@ -1,10 +1,10 @@
 class Kalk < Formula
   desc "kalk is a command line calculator app for developers."
   homepage "https://github.com/xoofx/kalk"
-  url "https://github.com/xoofx/kalk/releases/download/0.3.0/kalk.0.3.0.osx-x64.tar.gz"
-  sha256 "4bc92efd6422298597d806349e682283a1e9da7a3aa3fb7bb853edccff1e7906"
+  url "https://github.com/xoofx/kalk/releases/download/0.3.1/kalk.0.3.1.osx-x64.tar.gz"
+  sha256 "8a92c49ce8b2cd41c402a2fba6a143ce51952aa7a56d3b42f93f00da7188e675"
   license "BSD-2-Clause"
-  version "0.3.0"
+  version "0.3.1"
 
   def install
     bin.install "kalk"
